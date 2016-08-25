932560edc98903916de6b2da5fd1e47d1281571a
ayoolu03 <ayoolu03@yahoo.co.uk>
2016-08-25 17:56:36 +0100

    gmail update

--- send.php
+++ send.php
@@ -1,41 +1,52 @@
-<?
-require_once('PHPMailer/_lib/class.phpmailer.php');
+<?php
 
-$mail             = new PHPMailer();
+date_default_timezone_set('Etc/UTC');
+require 'PHPMailer/_lib/PHPMailerAutoload.php';
 
-$body             = file_get_contents('contents.html');
-$body             = eregi_replace("[\]",'',$body);
-
-$mail->IsSMTP(); // telling the class to use SMTP
-$mail->Host       = "smtp.gmail.com"; // SMTP server
-$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
-                                           // 1 = errors and messages
-                                           // 2 = messages only
-$mail->SMTPAuth   = true;                  // enable SMTP authentication
-$mail->Host       = "smtp.gmail.com"; // sets the SMTP server
-$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
-$mail->Username   = "developer@lifebank.ng"; // SMTP account username
-$mail->Password   = "part5bumf3";        // SMTP account password
-
-$mail->SetFrom('developer@lifebank.ng', 'Life dev');
-
-$mail->AddReplyTo('developer@lifebank.ng', 'Life dev');
-
-$mail->Subject    = "PHPMailer Test Subject via smtp, basic with authentication";
-
-$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
-
-$mail->MsgHTML($body);
-
-$address = "ayoolu03@gmail.com";
-$mail->AddAddress($address, "John Doe");
-
-$mail->AddAttachment("images/phpmailer.gif");      // attachment
-$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
-
-if(!$mail->Send()) {
-  echo "Mailer Error: " . $mail->ErrorInfo;
+$mail = new PHPMailer;
+//Tell PHPMailer to use SMTP
+$mail->isSMTP();
+//Enable SMTP debugging
+// 0 = off (for production use)
+// 1 = client messages
+// 2 = client and server messages
+$mail->SMTPDebug = 2;
+//Ask for HTML-friendly debug output
+$mail->Debugoutput = 'html';
+//Set the hostname of the mail server
+$mail->Host = 'smtp.gmail.com';
+// use
+// $mail->Host = gethostbyname('smtp.gmail.com');
+// if your network does not support SMTP over IPv6
+//Set the SMTP port number - 587 for authenticated TLS, a.k.a. RFC4409 SMTP submission
+$mail->Port = 587;
+//Set the encryption system to use - ssl (deprecated) or tls
+$mail->SMTPSecure = 'tls';
+//Whether to use SMTP authentication
+$mail->SMTPAuth = true;
+//Username to use for SMTP authentication - use full email address for gmail
+$mail->Username = "developer@lifebank.ng";
+//Password to use for SMTP authentication
+$mail->Password = "part5bumf3";
+//Set who the message is to be sent from
+$mail->setFrom('developer@lifebank.ng', 'dev Life');
+//Set an alternative reply-to address
+$mail->addReplyTo('developer@lifebank.ng', 'dev Life');
+//Set who the message is to be sent to
+$mail->addAddress('ayoolu03@gmail.com', 'John Doe');
+//Set the subject line
+$mail->Subject = 'PHPMailer GMail SMTP test';
+//Read an HTML message body from an external file, convert referenced images to embedded,
+//convert HTML into a basic plain-text alternative body
+$mail->msgHTML(file_get_contents('contents.html'), dirname(__FILE__));
+//Replace the plain text body with one created manually
+$mail->AltBody = 'This is a plain-text message body';
+//Attach an image file
+$mail->addAttachment('images/phpmailer_mini.png');
+//send the message, check for errors
+if (!$mail->send()) {
+    echo "Mailer Error: " . $mail->ErrorInfo;
 } else {
-  echo "Message sent!";
+    echo "Message sent!";
 }
 ?>
