3b96222f16019bdaa6978a43a91ac653a5efdd22
ayoolu03 <ayoolu03@yahoo.co.uk>
2016-08-25 17:50:16 +0100

    new work

---
+++ send.php
@@ -0,0 +1,41 @@
+<?
+require_once('PHPMailer/_lib/class.phpmailer.php');
+
+$mail             = new PHPMailer();
+
+$body             = file_get_contents('contents.html');
+$body             = eregi_replace("[\]",'',$body);
+
+$mail->IsSMTP(); // telling the class to use SMTP
+$mail->Host       = "smtp.gmail.com"; // SMTP server
+$mail->SMTPDebug  = 2;                     // enables SMTP debug information (for testing)
+                                           // 1 = errors and messages
+                                           // 2 = messages only
+$mail->SMTPAuth   = true;                  // enable SMTP authentication
+$mail->Host       = "smtp.gmail.com"; // sets the SMTP server
+$mail->Port       = 26;                    // set the SMTP port for the GMAIL server
+$mail->Username   = "developer@lifebank.ng"; // SMTP account username
+$mail->Password   = "part5bumf3";        // SMTP account password
+
+$mail->SetFrom('developer@lifebank.ng', 'Life dev');
+
+$mail->AddReplyTo('developer@lifebank.ng', 'Life dev');
+
+$mail->Subject    = "PHPMailer Test Subject via smtp, basic with authentication";
+
+$mail->AltBody    = "To view the message, please use an HTML compatible email viewer!"; // optional, comment out and test
+
+$mail->MsgHTML($body);
+
+$address = "ayoolu03@gmail.com";
+$mail->AddAddress($address, "John Doe");
+
+$mail->AddAttachment("images/phpmailer.gif");      // attachment
+$mail->AddAttachment("images/phpmailer_mini.gif"); // attachment
+
+if(!$mail->Send()) {
+  echo "Mailer Error: " . $mail->ErrorInfo;
+} else {
+  echo "Message sent!";
+}
+?>
